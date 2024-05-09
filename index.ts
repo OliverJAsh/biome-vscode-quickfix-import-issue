c8365670581fd666f31a93c36052a4884f8d6c24
Oliver Joseph Ash <oliverjash@gmail.com>
2024-05-09 09:47:34 +0100

    New test case

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-import { A } from 'a';
-import { B } from 'b';
-import { C } from 'c';
+// @ts-nocheck
+
+import * as APIClientError from 'app/services/api/APIClientError';
+import { TPhotoSlug, Codec } from 'app/services/api/Photo/types/PhotoSlug';
+
+APIClientError;
+export type F = TPhotoSlug;
 
 // edit this comment and save the file
